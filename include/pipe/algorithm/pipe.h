7940e517b944abda79da3c30e005188293e94b51
Vlad Riscutia <vladris@outlook.com>
2016-11-08 19:28:57 -0800

    Minor edit: remove needles "_"

--- include/pipe/algorithm/pipe.h
+++ include/pipe/algorithm/pipe.h
@@ -4,7 +4,7 @@
 
 namespace pipe { namespace algorithm { namespace details {
 
-template <typename Generator, typename Algorithm, typename _ = std::enable_if<pipe::is_generator<Generator>::value>::type>
+template <typename Generator, typename Algorithm, typename = std::enable_if<pipe::is_generator<Generator>::value>::type>
 auto operator|(Generator& gen, Algorithm&& algorithm)
 {
     return Algorithm::apply(std::move(gen), std::move(algorithm));
